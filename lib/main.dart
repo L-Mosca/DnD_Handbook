3df4bda99013be43d6e96e22ab481d8fa7e17f0f
Lucas-Mosca <110690274+Lucas-Mosca@users.noreply.github.com>
2023-05-16 23:29:06 -0300

    Feature: Add firebase flavors and app launcher icon.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
-import 'package:flutter/widgets.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
-
 import 'flavor/build_flavor.dart';
 import 'flavor/flavor.dart';
 
 void main() async {
-  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   buildFlavor(Flavor.develop);
 }
